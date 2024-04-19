a26ee8bbffbd85a66328f9167549319649372519
luyublog <luyublog@gmail.com>
2024-04-19 22:14:13 +0800

    #add pinecone交互

--- src/main/java/com/luyublog/aidemo/controller/OllamaController.java
+++ src/main/java/com/luyublog/aidemo/controller/OllamaController.java
@@ -3,9 +3,12 @@ package com.luyublog.aidemo.controller;
 import org.springframework.ai.chat.ChatResponse;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingClient;
 import org.springframework.ai.embedding.EmbeddingResponse;
 import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,11 +30,13 @@ import java.util.Map;
 public class OllamaController {
     private final OllamaChatClient ollamaChatClient;
     private final EmbeddingClient embeddingClient;
+    private final VectorStore vectorStore;
 
     @Autowired
-    public OllamaController(OllamaChatClient ollamaChatClient, EmbeddingClient embeddingClient) {
+    public OllamaController(OllamaChatClient ollamaChatClient, EmbeddingClient embeddingClient, VectorStore vectorStore) {
         this.ollamaChatClient = ollamaChatClient;
         this.embeddingClient = embeddingClient;
+        this.vectorStore = vectorStore;
     }
 
     @GetMapping("/ai/generate")
@@ -49,5 +54,20 @@ public class OllamaController {
     public Map embed(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
         EmbeddingResponse embeddingResponse = this.embeddingClient.embedForResponse(List.of(message));
         return Map.of("embedding", embeddingResponse);
+    }
+
+    @GetMapping("/ai/embedding/query")
+    public Map query(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        List<Document> documents = List.of(
+                new Document("Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!!", Map.of("meta1", "meta1")),
+                new Document("The World is Big and Salvation Lurks Around the Corner"),
+                new Document("You walk forward facing the past and you turn back toward the future.", Map.of("meta2", "meta2")));
+
+        // Add the documents
+        vectorStore.add(documents);
+
+        // Retrieve documents similar to a query
+        List<Document> results = vectorStore.similaritySearch(SearchRequest.query("Spring").withTopK(5));
+        return Map.of("result", results);
     }
 }
---
+++ src/main/java/com/luyublog/aidemo/vecstore/pinecone/PineconeVectorStoreConf.java
@@ -0,0 +1,50 @@
+package com.luyublog.aidemo.vecstore.pinecone;
+
+import org.springframework.ai.embedding.EmbeddingClient;
+import org.springframework.ai.vectorstore.PineconeVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * pinecone配置
+ * <p>
+ * &#064;author: east
+ * <p>
+ * &#064;date: 2024/4/19 21:54
+ */
+@Configuration
+public class PineconeVectorStoreConf {
+
+    @Value(value = "${spring.ai.vectorstore.pinecone.apiKey}")
+    String apikey;
+
+    @Value(value = "${spring.ai.vectorstore.pinecone.environment}")
+    String environment;
+
+    @Value(value = "${spring.ai.vectorstore.pinecone.projectId}")
+    String projectId;
+
+    @Value(value = "${spring.ai.vectorstore.pinecone.index-name}")
+    String indexName;
+
+    @Bean
+    public PineconeVectorStore.PineconeVectorStoreConfig pineconeVectorStoreConfig() {
+
+        return PineconeVectorStore.PineconeVectorStoreConfig.builder()
+                .withApiKey(apikey)
+                .withEnvironment(environment)
+//                .withProjectId("89309e6")
+//                .withIndexName("spring-ai-test-index")
+                .withProjectId(projectId)
+                .withIndexName(indexName)
+                .withNamespace("") // the free tier doesn't support namespaces.
+                .build();
+    }
+
+    @Bean
+    public VectorStore vectorStore(PineconeVectorStore.PineconeVectorStoreConfig config, EmbeddingClient ollamaEmbeddingClient) {
+        return new PineconeVectorStore(config, ollamaEmbeddingClient);
+    }
+}
